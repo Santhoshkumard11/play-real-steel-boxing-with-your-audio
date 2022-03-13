e9ee5abc41776b725832b4ad037e0cda2c8e9b08
Santhoshkumard11 <santhoshkdhana@gmail.com>
2022-03-13 14:37:00 +0530

    working initial transcription

---
+++ helpers/_constants.py
@@ -0,0 +1,25 @@
+KEYWORDS = [
+    "back step",
+    "uppercut",
+    "advance",
+    "engage",
+    "left",
+    "right",
+    "dodge",
+    "block",
+    "dash",
+    "dash back",
+]
+
+COMMANDS_AUDIO_MAPPING = {
+    "left jab": "",
+    "right jab": "",
+    "left hook": "",
+    "right hook": "",
+    "advance": "",
+    "dodge": "",
+    "dash back": "",
+    "dash forward": "",
+    "arms up": "",
+    "block": ""
+}
---
+++ helpers/actions.py
@@ -0,0 +1,5 @@
+from _constants import COMMANDS_AUDIO_MAPPING
+
+
+def make_move(text):
+    pass
